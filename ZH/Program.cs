0720e572c79f83bc5bd664dd574208a1417ed8d3
Servaretures <mmishak8@gmail.com>
2020-06-05 16:47:31 +0300

    Initial commit

---
+++ ZH/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Last num " + (num%10));
+            while (true)
+            {
+                if (num / 10 < 1)
+                {
+                    num = num % 10;
+                    break;
+                }
+                else
+                {
+                    num = num / 10;
+                }
+            }
+            Console.WriteLine("First num " + num);
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
